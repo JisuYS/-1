4f5647249830e9da134ed933850ac0e86d804481
JisuYS <128570029+JisuYS@users.noreply.github.com>
2023-05-16 06:27:34 +0000

    오늘은 switch 코드를 했다.

---
+++ .ccls-cache/@home@runner@kodeu1/mission_myscore.c
@@ -0,0 +1,15 @@
+// #include<stdio.h>
+// main(){
+//   int x;
+//   scanf("%d",&x);
+//   if(x>=90)
+//     printf("A학점 입니다.");
+//   else if (x>=80)
+//     printf("B학점 입니다.");
+//   else if(x>=70)
+//     printf("C학점 입니다.");
+//   else if(x>=60)
+//     printf("D학점 입니다.");
+//   else
+//     printf("F학점 입니다.");
+// }
